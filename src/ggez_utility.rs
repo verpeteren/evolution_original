08cbc3070d0311adba51de1bf6b2ea4cbaf36fa4
peter <peter.reijnders@verpeteren.nl>
2022-09-16 10:46:29 +0200

    patch: add tests for ggez_utility

--- src/ggez_utility.rs
+++ src/ggez_utility.rs
@@ -16,3 +16,40 @@ pub fn get_random_color(rng: &mut StdRng) -> Color {
     let b = rng.gen_range(0.0..1.0);
     Color::new(r, g, b, 1.0)
 }
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_lerp_color() {
+        let red = Color::RED;
+        let white = Color::WHITE;
+        let cyan = Color::CYAN;
+        let magenta = Color::MAGENTA;
+
+        let expected_1 = Color::new(1.0, 1.0, 1.0, 1.0);
+        assert_eq!(lerp_color(red, white, 1.0), expected_1);
+
+        let expected_2 = Color::new(1.0, 0.0, 0.0, 1.0);
+        assert_eq!(lerp_color(white, red, 1.0), expected_2);
+
+        let expected_3 = Color::new(0.54, 0.45999998, 1.0, 1.0);
+        assert_eq!(lerp_color(cyan, magenta, 0.54), expected_3);
+
+        let expected_4 = Color::new(0.14445001, 0.85555, 1.0, 1.0);
+        assert_eq!(lerp_color(magenta, cyan, 0.85555), expected_4);
+    }
+
+    #[test]
+    fn test_get_random_color() {
+         let mut rng = StdRng::from_rng(rand::thread_rng()).unwrap();
+         let color = get_random_color(&mut rng);
+         assert!(color.r >= 0.0 && color.r <= 1.0);
+         assert!(color.g >= 0.0 && color.g <= 1.0);
+         assert!(color.b >= 0.0 && color.b <= 1.0);
+         assert_eq!(color.a, 1.0);
+    }
+
+}
